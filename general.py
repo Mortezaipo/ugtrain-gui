cde51b542d66834dbb96f9c42528e11b0abfadbe
Mortezaipo <mortezaipo@gmail.com>
2016-03-18 11:38:45 +0330

    Update some part - add menubar.

--- general.py
+++ general.py
@@ -21,25 +21,49 @@ class General(gtk.Window):
         self.set_title("UGTrain - General")
         self.set_size_request(320, 370)
         self.set_position(gtk.WIN_POS_CENTER)
+        self.set_border_width(10)
+
+        # Manu for main window
+        menu = gtk.MenuBar()
+        filemenu = gtk.Menu()
+        filem = gtk.MenuItem("File")
+        filem.set_submenu(filemenu)
+        close = gtk.MenuItem("Exit")
+        close.connect("activate", gtk.main_quit)
+        new = gtk.MenuItem("New")
+        open = gtk.MenuItem("Open")
+        filemenu.append(new)
+        filemenu.append(open)
+        filemenu.append(close)
+        menu.append(filem)
 
         # Table for main window
-        main_table = gtk.Table(3, 1, False)
+        main_table = gtk.Table(2, 1, False)
+        main_table.set_row_spacings(10)
         self.sg_table = gtk.Table(4, 2, False)
         self.gs_table = gtk.Table(3, 2, False)
-        
-        self.draw_start_game()
-        self.draw_general_settings()
+
         # Frame for start game and general settings
         sg = gtk.Frame("Start game")
         gs = gtk.Frame("General settings")
+        sg_box = gtk.HBox()
+        gs_box = gtk.HBox()
+        body_box = gtk.VBox(False, 2)
         
         # Connect items
-        sg.add(self.sg_table)
-        gs.add(self.gs_table)
+        sg_box.pack_start(self.sg_table, padding=10)
+        gs_box.pack_start(self.gs_table, padding=10)
+        sg.add(sg_box)
+        gs.add(gs_box)
         main_table.attach(sg, 0, 1, 0, 1)
         main_table.attach(gs, 0, 1, 1, 2)
-        self.add(main_table)
+        self.draw_start_game()
+        self.draw_general_settings()
         
+        body_box.pack_start(menu, False, False, 0)
+        body_box.pack_end(main_table)
+        self.add(body_box)
+
         self.connect("destroy", gtk.main_quit)
         self.show_all()
         
