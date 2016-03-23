ebffad360e42743b5c655ee30ef6f9536883f2ef
Mortezaipo <mortezaipo@gmail.com>
2016-03-24 01:56:37 +0430

    Update several parts of static window , update edit mode.

--- static.py
+++ static.py
@@ -1,160 +1,234 @@
 import gtk
+import settings
 
 
 class Static(gtk.Window):
+    """Static window."""
+    
+    # Edit mode for editing list items.
+    edit_mode = None
+    
     def __init__(self):
         super(Static, self).__init__()
         
         # Set window settings
         self.set_size_request(1120, 300)
         self.set_title("UGTrain - Static Form")
-        self.set_border_width(10)
+        self.set_border_width(settings.window_border)
         
         # Create layouts
         box = gtk.VBox(False, 5)
-        self.setting = gtk.Table(2, 7, False)
-        self.setting.set_col_spacings(10)
+        self.setting_table = gtk.Table(2, 7, False)
+        self.setting_table.set_col_spacings(settings.table_col_space)
         
         # Draw elements
-        self.draw_elements()
-        savebtn = gtk.Button("Save static data")
-        addarg = gtk.Button("Add new args")
-        fbox = gtk.Fixed()
+        #self.define_elements()
         
-        # Connect element to signals
-        addarg.connect("clicked", self.new_data_win)
-        
-        # Create List Store
-        swin = gtk.ScrolledWindow()
-        swin.set_shadow_type(gtk.SHADOW_ETCHED_IN)
-        self.lstore = gtk.ListStore(str, str, str, str, str, str)
-        self.tree = gtk.TreeView(self.lstore)
-        self.tree.connect("row-activated", self.edit_list_item)
-        for i in range(6):
-            row = gtk.CellRendererText()
-            cell = gtk.TreeViewColumn("Arg %d" % i, row, text=i)
-            self.tree.append_column(cell)
-        
-        # Connect elements to layouts
-        swin.add(self.tree)
-        fbox.put(savebtn, 0, 0)
-        fbox.put(addarg, 120, 0)
-        box.pack_start(self.setting, False, False, 0)
-        box.pack_start(swin, True, True, 0)
-        box.pack_end(fbox, False, False, 0)
-        self.add(box)
-        
-        self.show_all()
-        
-    def draw_elements(self):
-        elements = (
+        # Define Config elements
+        self.conf_elements = (
                     ('Value name', gtk.Entry()),
                     ('Abs Address', gtk.Entry()),
                     ('Data Type', gtk.Entry()),
                     ('Check', gtk.CheckButton()),
                     ('Wish Value', gtk.Entry()),
                     ('Key Binding', gtk.Entry()),
-                    ('Act State', gtk.Entry())
-                   )
+                    ('Act State', gtk.Entry()),
+        )
+        
         i = 0
-        for label, element in elements:
-            self.setting.attach(gtk.Label(label), i, i+1, 0, 1)
-            self.setting.attach(element, i, i+1, 1, 2)
-            i+= 1
-            
-            
-    def new_data_win(self, widget):
-        new_data = gtk.Window()
-        new_data.set_size_request(310, 450)
-        new_data.set_border_width(10)
+        for label, element in self.conf_elements:
+            self.setting_table.attach(gtk.Label(label), i, i+1, 0, 1)
+            self.setting_table.attach(element, i, i+1, 1, 2)
+            i += 1
+        
+        save_btn = gtk.Button("Save static data")
+        add_arg = gtk.Button("Add new args")
+        fix = gtk.Fixed()
+        
+        # Connect element to signals
+        add_arg.connect("clicked", self.data_win)
+        
+        # Create List Store
+        scrolled_win = gtk.ScrolledWindow()
+        scrolled_win.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        self.list_store = gtk.ListStore(str, str, str, str, str, str)
+        self.tree = gtk.TreeView(self.list_store)
+        self.tree.connect("row-activated", self.edit_data_win)
+        for i in range(6):
+            row = gtk.CellRendererText()
+            cell = gtk.TreeViewColumn("Arg %d" % i, row, text=i)
+            self.tree.append_column(cell)
+        
+        # Connect elements to layouts
+        scrolled_win.add(self.tree)
+        fix.put(save_btn, 0, 0)
+        fix.put(add_arg, 120, 0)
+        box.pack_start(self.setting_table, False, False, 0)
+        box.pack_start(scrolled_win, True, True, 0)
+        box.pack_end(fix, False, False, 0)
+        self.add(box)
+        self.show_all()
+        
+    def define_elements(self, form_data=None):
+        """Declare all required elements - Config, Argument, Check elements."""
+        
+        # Define data
+        self.data_types = ['i8', 'i16', 'i32', 'i64', 'u8', 'u16', 'u32', 'u64', 'f32', 'f64']
+        self.compare_types = ['I', 'G', 'E']
+        self.other_data_types = ['Futhur', 'Check']
+                
+        # Define Argument elements
+        self.arg_elements = (
+                ('a1', gtk.Label('Val name *'), gtk.Entry()),
+                ('a2', gtk.Label('Abs address *'), gtk.Entry()),
+                ('a3', gtk.Label('Data type *'), gtk.combo_box_new_text()),
+                ('a4', gtk.Label('Check'), gtk.CheckButton()),
+                ('a5', gtk.Label('Watch'), gtk.CheckButton()),
+        )
+        
+        self.check_elements = (
+                ('c1', gtk.Label('Check'), gtk.CheckButton()),
+                ('c2', gtk.Label('Abs address *'), gtk.Entry()),
+                ('c3', gtk.Label('Data type *'), gtk.combo_box_new_text()),
+                ('c4', gtk.Label('*'), gtk.combo_box_new_text()),
+                ('c5', gtk.Label('Check value'), gtk.CheckButton()),
+                ('c6', gtk.Label('*'), gtk.combo_box_new_text()),
+        )
+        
+        for i in self.data_types:
+            self.arg_elements[2][2].append_text(i)
+            self.check_elements[2][2].append_text(i)
+        
+        for i in self.compare_types:
+            self.check_elements[3][2].append_text(i)
+        
+        for i in self.other_data_types:
+            self.check_elements[5][2].append_text(i)
+        
+        # FIXME: need optimize
+        if form_data:
+            self.arg_elements[0][2].set_text(form_data[0])
+            self.arg_elements[1][2].set_text(form_data[1])
+            if form_data[2] != None:
+                self.arg_elements[2][2].set_active(self.data_types.index(form_data[2]))
+            if form_data[3] == 'True':
+                self.arg_elements[3][2].activate()
+            #self.arg_elements[4][2].set_mode(form_data[4])
+
+
+    def data_win(self, widget=None, form_data=None):
+        """Data window to add/edit list items."""
+        
+        # Create window
+        data_win = gtk.Window()
+        data_win.set_size_request(310, 480)
+        data_win.set_border_width(10)
+        data_win.connect("destroy", self.close_data_win)
         
         # Create layouts
         box = gtk.VBox(False, 2)
-        fbox = gtk.Fixed()
+        fix = gtk.Fixed()
         abox = gtk.HBox()
         cbox = gtk.HBox()
         self.arg_table = gtk.Table(5, 2, False)
-        self.arg_table.set_row_spacings(5)
+        self.arg_table.set_row_spacings(settings.table_row_space)
         self.check_table = gtk.Table(6, 2, False)
-        self.check_table.set_row_spacings(5)
+        self.check_table.set_row_spacings(settings.table_row_space)
         arg_frame = gtk.Frame('Argument')
         chk_frame = gtk.Frame('Check')
         
-        # Create elements
-        save_btn = gtk.Button('Add')
-        save_btn.connect("clicked", self.add_list_item)
-        self.draw_new_data_elements()
+        if self.edit_mode:
+            save_btn = gtk.Button('Update')
+            save_btn.connect("clicked", self.edit_list_item)
+        else:
+            save_btn = gtk.Button('Add')
+            save_btn.connect("clicked", self.add_list_item)
+        
+        if not self.edit_mode:
+            self.define_elements()
+        else:
+            self.define_elements(form_data)
+            
+        i = 0
+        for _, label, element in self.arg_elements:
+            self.arg_table.attach(label, 0, 1, i, i+1)
+            self.arg_table.attach(element, 1, 2, i, i+1)
+            i += 1
+        i = 0
+        for _, label, element in self.check_elements:
+            self.check_table.attach(label, 0, 1, i, i+1)
+            self.check_table.attach(element, 1, 2, i, i+1)
+            i += 1
         
         # Attach elements to layouts
         abox.pack_start(self.arg_table, padding=10)
         cbox.pack_start(self.check_table, padding=10)
         arg_frame.add(abox)
         chk_frame.add(cbox)
-        fbox.put(save_btn, 0, 0)
+        fix.put(save_btn, 0, 0)
         box.pack_start(arg_frame)
         box.pack_start(chk_frame)
-        box.pack_end(fbox)
-        new_data.add(box)
-        new_data.show_all()
+        box.pack_end(fix)
+        data_win.add(box)
+        data_win.show_all()
         
+    def close_data_win(self, widget):
+        self.edit_mode = None
+        widget.do_destroy
         
-    def draw_new_data_elements(self):
-        self.arg_elements = (
-            ('Val name', gtk.Entry()),
-            ('Abs address', gtk.Entry()),
-            ('Data type', gtk.Entry()), #FIXME: should change to select box
-            ('Check', gtk.CheckButton()),
-            ('Watch', gtk.CheckButton()),
-        )
-        
-        i = 0
-        for label, element in self.arg_elements:
-            self.arg_table.attach(gtk.Label(label), 0, 1, i, i+1)
-            self.arg_table.attach(element, 1, 2, i, i+1)
-            i += 1
-            
-            
-        c1 = gtk.combo_box_new_text()
-        c1.append_text('I')
-        c1.append_text('G')
-        c1.append_text('E')
-        
-        c2 = gtk.combo_box_new_text()
-        c2.append_text('Futhur')
-        c2.append_text('Check')
-        
-        self.check_elements = (
-            ('Check', gtk.CheckButton()),
-            ('Abs address', gtk.Entry()),
-            ('Data type', gtk.Entry()), #FIXME: should change to select box
-            ('', c1),
-            ('Check value', gtk.CheckButton()),
-            ('', c2),
-        )
-        
-        i = 0
-        for label, element in self.check_elements:
-            self.check_table.attach(gtk.Label(label), 0, 1, i, i+1)
-            self.check_table.attach(element, 1, 2, i, i+1)
-            i += 1
-
     def add_list_item(self, widget):
-        self.lstore.append((self.arg_elements[0][1].get_text(),
-                            self.arg_elements[1][1].get_text(),
-                            self.arg_elements[2][1].get_text(),
-                            str(self.arg_elements[3][1].get_active()),
-                            str(self.arg_elements[4][1].get_active()),
+        if not self.validate_fields():
+            return False
+        
+        self.list_store.append((
+                            self.arg_elements[0][2].get_text(),
+                            self.arg_elements[1][2].get_text(),
+                            self.arg_elements[2][2].get_active_text(),
+                            str(self.arg_elements[3][2].get_active()),
+                            str(self.arg_elements[4][2].get_active()),
                             '',
                             ))
         
-        self.lstore.append((str(self.check_elements[0][1].get_active()),
-                            self.check_elements[1][1].get_text(),
-                            self.check_elements[2][1].get_text(),
-                            self.check_elements[3][1].get_active_text(),
-                            str(self.check_elements[4][1].get_active()),
-                            self.check_elements[5][1].get_active_text(),                            
+        self.list_store.append((
+                            str(self.check_elements[0][2].get_active()),
+                            self.check_elements[1][2].get_text(),
+                            self.check_elements[2][2].get_active_text(),
+                            self.check_elements[3][2].get_active_text(),
+                            str(self.check_elements[4][2].get_active()),
+                            self.check_elements[5][2].get_active_text(),
                             ))
         
-    def edit_list_item(self, widget, row, col):
+    def edit_data_win(self, widget, row, col):
         widget = widget.get_model()
-        print widget[row][0]
+        #self.arg_elements[0][1].set_text(widget[row][0])
+        self.edit_mode = row
+        #print self.arg_elements[0][1].get_text()
+        form_data = (widget[row][0], widget[row][1], widget[row][2], widget[row][3], widget[row][4])
+        self.data_win(form_data=form_data)
+        
+    def edit_list_item(self, widget):
+        pass
+    
+    def validate_fields(self):
+        return True
+        for i in range(3):
+            if(self._arg_elements[i][1].get_text() == ""):
+                d = gtk.MessageDialog(self, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, "Enter * fields data in Argument part.");
+                d.run()
+                d.destroy()
+                return False
+            
+        for i in [1, 2]:
+            if(self._check_elements[i][1].get_text() == ""):
+                d = gtk.MessageDialog(self, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, "Enter * fields data in Check part.");
+                d.run()
+                d.destroy()
+                return False
+            
+        for i in [3, 5]:
+            if(self._check_elements[i][1].get_active_text() == ""):
+                d = gtk.MessageDialog(self, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, "Choice * fields data in Check part.");
+                d.run()
+                d.destroy()
+                return False
+        return True
