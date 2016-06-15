82aa9c8d174437cbeeb1b98f961a4ad6cbbdd42c
Mortezaipo <mortezaipo@gmail.com>
2016-06-16 01:06:00 +0430

    Add simple gtk window in main file.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include <gtk/gtk.h>
 
 int main(int argc, char* argv[])
 {
+    GtkWidget *win;
+    gtk_init(&argc, &argv);
+
+    win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(win), "UGTrain-GUI");
+    gtk_widget_show(win);
+
+    gtk_main();
     return 0;
 }
