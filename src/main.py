5195d26319a380c6a1a5a533f7b47a16f63b3857
Mortezaipo <mortezaipo@gmail.com>
2016-07-08 14:47:11 +0430

    Update main file - remove gtk main call and file docstring.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python
-import gtk
+#!/usr/bin/env python2
+"""UGTrain-GUI is a desktop application to manage UGTrain config files.
+Author: Morteza Nourelahi Alamdari (Mortezaipo)
+License: GNU GENERAL PUBLIC LICENSE Version 3
+"""
 from general import General
 
 
 class Main:
-    
     def __init__(self):
         self.general = General()
         self.general.draw_gui()
-        
 
 if __name__ == "__main__":
     Main()
-    gtk.main()
